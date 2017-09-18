2948d4e17b22ee3ebb43e0ec4c4988c46bdd0e83
nickylimjj <nickylimjj.nl@gmail.com>
2017-09-18 14:12:34 +0200

    implemented read_graph

--- independentset/independentset.py
+++ independentset/independentset.py
@@ -1,14 +1,28 @@
 # independentset.py
 import sys
 import os
+import networkx as nx
 
-def read_graph(filename):
+def read_graph(pathname):
     """
     DESC:   reads a graph using the lab's input format
     INPUT:  string "gXX.in" in data/
     OUTPUT: graph G
     """
-    raise NotImplementedError
+
+    print("reading file: {}".format(pathname))
+    f = open(pathname, 'r')
+    n = int(f.readline())                   # number of vertices
+    G = nx.Graph()
+
+    for src in range(n):
+        G.add_node(src)                     # add a vertices
+        edges = str.split(f.readline()," ")[:-1]
+        for (dest, e) in enumerate(edges):  # add all edges
+            if e == '1':
+                G.add_edge(src, dest)
+
+    return G
 
 def alpha(G):
     """
@@ -22,9 +36,14 @@ def alpha(G):
 if __name__ == "__main__":
 
     filename = "g30.in"
+    filepath = os.path.join(os.getcwd(),'data',filename)
     
     # read graph
-    G = read_graph(filename)
+    G = read_graph(filepath)
+    n = len(G.nodes())
+    m = len(G.edges())
+    print("num nodes:\t{}".format(n))
+    print("num edges:\t{}".format(m))
 
     # run algorithm R0 to find size of largest independent set
     alpha = MIS(G)
