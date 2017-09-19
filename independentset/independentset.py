3cd5c3b842afe257a3fc553abdf108b0cf17d85c
nickylimjj <nickylimjj.nl@gmail.com>
2017-09-19 14:08:38 +0200

    buggy R2

--- independentset/independentset.py
+++ independentset/independentset.py
@@ -131,6 +131,103 @@ def R1(G):
 
     return max(include_u, exclude_u)
 
+def R2(G):
+    """
+    DESC:   analyzes a vertices-weighted graph G and returns the Max
+            Independent Set (MIS) using algorithm R2
+    INPUT:  graph G
+    OUTPUT: MIS value alpha
+    """
+
+    global counter
+    counter += 1
+
+    # base case: V(G) = 0
+    if G.nodes() == []:
+        return 0
+
+    # dict of degrees (value) indexed by vertex (key)
+    degs = G.degree()
+
+    if 2 in degs.values():
+        u = degs.keys()[degs.values().index(2)] # get vertex with deg = 2
+    elif 1 in degs.values():
+        u = degs.keys()[degs.values().index(1)] # get vertex with deg = 1
+    else:
+        u = max(degs, key=degs.get)             # get vertex with max degree
+
+    neighbors = G.neighbors(u) 
+
+    edges_to_u = G.edges(u)                     # saved for restoration
+    G.remove_node(u)
+
+
+    # R2 mod: vertex u has two neighbors x,y and xy is an edge
+    if len(neighbors) == 2:
+        # neighbors of x & y without u (for R2)
+        neighbors_of_x_y = G.nodes(neighbors[0]) + G.nodes(neighbors[1])
+        print neighbors_of_x_y
+
+        edges_to_neighbors = G.edges(neighbors) # saved for restoration excld. u
+
+        # if xy is an edge, add u and remove x & y
+        if (neighbors[0],neighbors[1]) in G.edges():
+            G.remove_nodes_from(neighbors)
+            alpha = 1 + R2(G)
+            # restore graph
+            G.add_nodes_from([u] + neighbors)
+            G.add_edges_from(edges_to_u + edges_to_neighbors)
+            return alpha
+        # else add new vertex z, connect to x & y neighbors (less u)
+        # and remove v,x,y
+        else:
+            G.remove_nodes_from(neighbors)
+            # the new vertex z taking on u's key
+            z = u
+            G.add_node(z)   
+            # connect z to all of x & y neighbors, excluding u
+            #G.add_edges_from(zip([z for i in range(len(neighbors_of_x_y))],
+                                 #neighbors_of_x_y)) 
+
+            alpha = 1 + R2(G)
+
+            # restore graph
+            G.remove_node(z)
+            G.add_nodes_from([u] + neighbors)
+            G.add_edges_from(edges_to_u + edges_to_neighbors)
+            return alpha
+
+    # R1 mod: add vertex u and exclude neighbors
+    if len(neighbors) == 1:
+        edges_to_neighbors = G.edges(neighbors) # saved for restoration
+        G.remove_nodes_from(neighbors)
+        alpha = 1 + R2(G)
+        # restore graph
+        G.add_nodes_from([u] + neighbors)
+        G.add_edges_from(edges_to_u + edges_to_neighbors)
+        return alpha
+
+    # loner nodes added at no penalty
+    if len(neighbors) == 0:
+        alpha = 1 + R2(G)
+        # restore graph
+        G.add_node(u)
+        G.add_edges_from(edges_to_u)
+        return alpha
+
+    exclude_u = R2(G)
+
+    edges_to_neighbors = G.edges(neighbors) # saved for restoration
+    G.remove_nodes_from(neighbors)
+
+    include_u = 1 + R2(G)
+
+    # restore graph
+    G.add_nodes_from([u] + neighbors)
+    G.add_edges_from(edges_to_u + edges_to_neighbors)
+
+
+    return max(include_u, exclude_u)
 if __name__ == "__main__":
 
     # parse cmdline args
@@ -155,6 +252,7 @@ if __name__ == "__main__":
     algorithms = {
             'R0': R0,
             'R1': R1,
+            'R2': R2
             }
     # run algorithm R0 to find size of largest independent set
     for alg in args.algorithm:
