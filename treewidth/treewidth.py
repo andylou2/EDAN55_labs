5159bde271576d028b2abbe09db4a462a3afbe14
andylou2 <andylou2@illinois.edu>
2017-09-26 18:02:48 +0200

    rooting tree

--- treewidth/treewidth.py
+++ treewidth/treewidth.py
@@ -4,6 +4,8 @@ import os
 import argparse
 import random
 import networkx as nx
+import numpy as np
+import json
 
 def read_graphs(pathname):
     """
@@ -93,6 +95,28 @@ def read_T_ (pathname):
 
     return T
 
+
+def root_tree(t):
+    """
+    :param t: tree
+    :return: dictionary of dictionary holding the children, bag contents, and table of each node in the tree
+    """
+    tree_data = {}
+    tree = nx.dfs_tree(T, 1)
+    for i in range(1, len(t.nodes()) + 1):
+        tree_data[i] = {}
+        print(tree.neighbors(i))
+        tree_data[i]['children'] = tree.neighbors(i)                         # returns array of the ith nodes children
+        tree_data[i]['vertices'] = parse_contents(i, t)                      # parses the node contents from int to string
+        tree_data[i]['table'] = np.zeros(2**len(tree_data[i]['vertices']) + 1).tolist()
+    return tree, tree_data
+
+
+def parse_contents(i, t):
+    s = t.nodes(data=True)[i - 1][1]['contents']
+    return list(map(lambda x: int(x), str.split(s, " ")))
+
+
 if __name__ == "__main__":
 
     # parse cmdline args
@@ -118,4 +142,11 @@ if __name__ == "__main__":
     print("T")
     print("\tnum nodes:\t{}".format(t_n))
     print("\tnum edges:\t{}".format(t_m))
+
+    # t, x = root_tree(T)
+    # f = open("test.txt", 'w+')
+
+    # json.dump(x, f, sort_keys=True, indent=4, separators=(',', ': '))
+    # f.close()
+
 
